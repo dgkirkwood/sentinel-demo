4287fc9b6c7802433e7471be97cffff8a2ee4c12
dgkirkwood <daniel.g.kirkwood@gmail.com>
2020-03-26 09:27:09 +1100

    fix tag assignment

--- main.tf
+++ main.tf
@@ -22,7 +22,7 @@ data "aws_ami" "ubuntu" {
 resource "aws_instance" "web" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t2.micro"
-  tags {
+  tags = {
     Name = "my_instance"
   }
 }
